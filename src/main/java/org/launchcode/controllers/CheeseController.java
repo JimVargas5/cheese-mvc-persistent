8537cfee553d377c34b0d158ae4fb57efec701d4
Jim Vargas <jimball1@hotmail.com>
2017-09-16 16:47:08 -0700

    Bonus edit functionality added. Improved Menu access.

--- src/main/java/org/launchcode/controllers/CheeseController.java
+++ src/main/java/org/launchcode/controllers/CheeseController.java
@@ -5,6 +5,7 @@ import org.launchcode.models.Cheese;
 import org.launchcode.models.Menu;
 import org.launchcode.models.data.CategoryDao;
 import org.launchcode.models.data.CheeseDao;
+import org.launchcode.models.data.MenuDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -30,12 +32,15 @@ public class CheeseController {
     @Autowired
     CategoryDao categoryDao;
 
+    @Autowired
+    MenuDao menuDao;
+
     // Request path: /cheese
     @RequestMapping(value = "")
     public String index(Model model) {
 
         model.addAttribute("cheeses", cheeseDao.findAll());
-        model.addAttribute("title", "My Cheeses");
+        model.addAttribute("title", "The Cheeses");
 
         return "cheese/index";
     }
@@ -45,22 +50,36 @@ public class CheeseController {
         model.addAttribute("title", "Add Cheese");
         model.addAttribute(new Cheese());
         model.addAttribute("categories", categoryDao.findAll());
+        model.addAttribute("menus", menuDao.findAll());
         return "cheese/add";
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@ModelAttribute  @Valid Cheese newCheese, Errors errors,
-                                       @RequestParam int categoryId, Model model) {
+    public String processAddCheeseForm(@ModelAttribute  @Valid Cheese newCheese, Errors errors, Model model,
+                                       @RequestParam int categoryId,
+                                       @RequestParam(value="menuIds", required = false) int[] menuIds) {
 
         if (errors.hasErrors()) {
             model.addAttribute("title", "Add Cheese");
             model.addAttribute("categories", categoryDao.findAll());
+            model.addAttribute("menus", menuDao.findAll());
             return "cheese/add";
         }
-
         Category aCategory = categoryDao.findOne(categoryId);
         newCheese.setCategory(aCategory);
         cheeseDao.save(newCheese);
+
+        if (menuIds != null) {
+            ArrayList<Integer> newMenuIds = new ArrayList<>();
+            for (int menuId : menuIds) {
+                Integer integerObj = new Integer(menuId);
+                newMenuIds.add(integerObj);
+            }
+            for (Menu menu : menuDao.findAll(newMenuIds)) {
+                menu.addItem(newCheese);
+                menuDao.save(menu);
+            }
+        }
         return "redirect:";
     }
 
@@ -94,6 +113,79 @@ public class CheeseController {
         model.addAttribute("cheeses", cheeses);
         model.addAttribute("title", "Cheeses in Category: "+aCategory.getName());
         return "cheese/index";
+    }
+
+    /*@RequestMapping(value= "edit-choose", method= RequestMethod.GET)
+    public String chooseACheeseToEdit(Model model){
+        model.addAttribute("title", "Choose a cheese to edit:");
+        model.addAttribute("cheeses", cheeseDao.findAll());
+
+        return "cheese/edit-choose";
+    }*/
+
+    @RequestMapping(value = "edit", method = RequestMethod.GET)
+    public String displayEditForm(Model model, @RequestParam int id){
+        model.addAttribute("title", "Edit "+cheeseDao.findOne(id).getName());
+        model.addAttribute(new Cheese());
+        model.addAttribute("categories", categoryDao.findAll());
+        model.addAttribute("menus", menuDao.findAll());
+        model.addAttribute("givenCheese", cheeseDao.findOne(id));
+
+        return "cheese/edit";
+    }
+
+    @RequestMapping(value = "edit", method = RequestMethod.POST)
+    public String processEditForm(@ModelAttribute  @Valid Cheese tempCheese, Errors errors, Model model,
+                                  @RequestParam int categoryId, @RequestParam int givenCheeseId,
+                                  @RequestParam(value="menuIds", required = false) int[] menuIds){
+        //check to see if tempCheese has valid fields
+        if (errors.hasErrors()){
+            model.addAttribute("title", "Edit "+cheeseDao.findOne(givenCheeseId).getName());
+            model.addAttribute("categories", categoryDao.findAll());
+            model.addAttribute("menus", menuDao.findAll());
+            model.addAttribute("givenCheese", cheeseDao.findOne(givenCheeseId));
+            return "cheese/edit";
+        }
+        //update givenCheese to tempCheese's fields
+        Cheese givenCheese = cheeseDao.findOne(givenCheeseId);
+        givenCheese.setCategory(categoryDao.findOne(categoryId));
+        givenCheese.setName(tempCheese.getName());
+        givenCheese.setDescription(tempCheese.getDescription());
+        cheeseDao.save(givenCheese);
+        //update necessary menus to drop/add givenCheese
+        if (menuIds != null) {
+            //set up desired and old menus into nice ArrayLists
+            ArrayList<Integer> desiredMenuIds = new ArrayList<>();
+            for (int menuId : menuIds) {
+                Integer integerObj = new Integer(menuId);
+                desiredMenuIds.add(integerObj);
+            }
+            ArrayList<Menu> desiredMenus = new ArrayList<>();
+            for (Integer desiredMenuId : desiredMenuIds){
+                desiredMenus.add(menuDao.findOne(desiredMenuId));
+            }
+            List<Menu> oldMenus = givenCheese.getMenus();
+            //do some loops to compare current vs desired
+            for (Menu currentMenu : menuDao.findAll()) {
+                if (oldMenus.contains(currentMenu)){
+                    if (!desiredMenus.contains(currentMenu)){
+                        currentMenu.removeItem(givenCheese);
+                        menuDao.save(currentMenu);
+                    }
+                }else {
+                    if (desiredMenus.contains(currentMenu)){
+                        currentMenu.addItem(givenCheese);
+                        menuDao.save(currentMenu);
+                    }
+                }
+            }
+        }else {
+            for (Menu menu : givenCheese.getMenus()){
+                menu.removeItem(givenCheese);
+                menuDao.save(menu);
+            }
+        }
+        return "redirect:/cheese";
     }
 
 }
--- src/main/java/org/launchcode/models/Menu.java
+++ src/main/java/org/launchcode/models/Menu.java
@@ -52,4 +52,19 @@ public class Menu {
     public void setCheeses(List<Cheese> cheeses) {
         this.cheeses = cheeses;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Menu menu = (Menu) o;
+
+        return id == menu.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
 }
